2cc2da439caa16068c67092e49f3f0c911575d52
syqwq-OMG <3422403944@qq.com>
2024-09-18 15:37:09 +0800

    done

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,13 @@ use std::{
     thread,
 };
 
+/// 作为线程任务传入的闭包
 type Job = Box<dyn FnOnce() + Send + 'static>;
+
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
-
 impl ThreadPool {
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
@@ -20,7 +21,7 @@ impl ThreadPool {
         for id in 0..size {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        ThreadPool { workers, sender }
+        ThreadPool { workers, sender:Some(sender) }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -28,23 +29,46 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for worker in &mut self.workers {
+            drop(self.sender.take());
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
+/// 用于处理任务的结构
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
-
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
-            println!("Worker {id} got a job; executing.");
-
-            job();
+            let message = receiver.lock().unwrap().recv();
+            match message {
+                Ok(job)=>{
+                    println!("Worker {id} got a job; executing.");
+                    job();
+                },
+                Err(_)=>{
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
+            
         });
-        Worker { id, thread }
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
